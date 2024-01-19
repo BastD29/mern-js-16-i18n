d69fa49edd2a4c3f0917dcbf4fe0ca130a2668b1
BastD29 <bastiendaniel9@gmail.com>
2024-01-19 13:25:44 +0100

    modified keys to text

--- client/src/locales/en.ts
+++ client/src/locales/en.ts
@@ -1,10 +1,21 @@
+// export default {
+//   titles: {
+//     title1: "Title number one",
+//     title2: "Title number two",
+//   },
+//   descriptions: {
+//     description1: "This is description number one",
+//     description2: "This is description number two",
+//   },
+// };
+
 export default {
   titles: {
-    title1: "Title number one",
-    title2: "Title number two",
+    "Title number one": "Title number one",
+    "Title number two": "Title number two",
   },
   descriptions: {
-    description1: "This is description number one",
-    description2: "This is description number two",
+    "This is description number one": "This is description number one",
+    "This is description number two": "This is description number two",
   },
 };
--- client/src/locales/fr.ts
+++ client/src/locales/fr.ts
@@ -1,10 +1,21 @@
+// export default {
+//   titles: {
+//     title1: "Titre numéro un",
+//     title2: "Titre numéro deux",
+//   },
+//   descriptions: {
+//     description1: "Ceci est la description numéro un",
+//     description2: "Ceci est la description numéro deux",
+//   },
+// };
+
 export default {
   titles: {
-    title1: "Titre numéro un",
-    title2: "Titre numéro deux",
+    "Title number one": "Titre numéro un",
+    "Title number two": "Titre numéro deux",
   },
   descriptions: {
-    description1: "Ceci est la description numéro un",
-    description2: "Ceci est la description numéro deux",
+    "This is description number one": "Ceci est la description numéro un",
+    "This is description number two": "Ceci est la description numéro deux",
   },
 };
